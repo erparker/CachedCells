6e939f4fbf4732e294079e4bd9f4491cbf51d133
erparker <ericparker1987@gmail.com>
2014-12-10 16:09:55 -0700

    initial commit

--- CachingTableViewCells/ViewController.m
+++ CachingTableViewCells/ViewController.m
@@ -10,18 +10,50 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSMutableDictionary *cells;
+
+@property (nonatomic, strong) UITableView *tableView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.cells = [[NSMutableDictionary alloc] init];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //Don't create a new cell if your cell is already in memory. Return it from your dictionary of cells
+    if ([self.cells objectForKey:[NSString stringWithFormat:@"cell%ld", (long)indexPath.row]]) {
+        return [self.cells objectForKey:[NSString stringWithFormat:@"cell%ld", (long)indexPath.row]];
+    }
+    
+    //Register a new identifier for each cell to avoid dequeueReusableCellWithIdentifier: reusing an old one
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:[NSString stringWithFormat:@"cell%ld", (long)indexPath.row]];
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[NSString stringWithFormat:@"cell%ld", (long)indexPath.row]];
+    
+    //Do any additional cell setup here
+    
+    [self.cells setObject:cell forKey:[NSString stringWithFormat:@"cell%ld", (long)indexPath.row]];
+    
+    
+    return cell;
+}
+
 
 @end
