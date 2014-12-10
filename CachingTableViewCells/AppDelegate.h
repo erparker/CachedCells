d33147f83aa074416ba767936c23ee4debb1b76d
erparker <ericparker1987@gmail.com>
2014-12-10 16:03:46 -0700

    Initial Commit

---
+++ CachingTableViewCells/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CachingTableViewCells
+//
+//  Created by Eric Parker on 12/10/14.
+//  Copyright (c) 2014 testing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CachingTableViewCells/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CachingTableViewCells
+//
+//  Created by Eric Parker on 12/10/14.
+//  Copyright (c) 2014 testing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
